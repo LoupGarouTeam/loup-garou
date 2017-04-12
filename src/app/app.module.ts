343840d551fcfa2856f0b9c4b0731e6bd939c8d1
phlesaux <ph.lesaux@outlook.com>
2017-04-12 16:13:15 +0200

    Regexp sur pseudo + validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { RoleComponent } from './role/role.component';
-import { TimerComponent } from './timer/timer.component';
-import { TchatComponent } from './tchat/tchat.component';
-import { LobbyComponent } from './lobby/lobby.component';
-import { AuthComponent } from './auth/auth.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpModule} from '@angular/http';
+import {AppComponent} from './app.component';
+import {RoleComponent} from './role/role.component';
+import {TimerComponent} from './timer/timer.component';
+import {TchatComponent} from './tchat/tchat.component';
+import {LobbyComponent} from './lobby/lobby.component';
+import {AuthComponent} from './auth/auth.component';
 
 const appRoutes: Routes = [
-  { path: 'login', component: AuthComponent },
-  { path: 'test', component: TchatComponent },
-  { path: 'roletest', component: RoleComponent },
-  { path: 'timer', component: TimerComponent },
-  { path: '',   redirectTo: '/login', pathMatch: 'full' }
+    {path: 'login', component: AuthComponent},
+    {path: 'test', component: TchatComponent},
+    {path: 'roletest', component: RoleComponent},
+    {path: 'timer', component: TimerComponent},
+    {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RoleComponent,
-    TimerComponent,
-    TchatComponent,
-    AuthComponent,
-    LobbyComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(appRoutes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RoleComponent,
+        TimerComponent,
+        TchatComponent,
+        AuthComponent,
+        LobbyComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
