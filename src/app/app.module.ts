eb65ae39ef7a3b61e5de9717dc8dd2d3e99914d3
lucas-wilmart <wilmart.lucas@gmail.com>
2017-04-12 14:02:20 +0200

    On aurait du aller en WordPress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { RoleComponent } from './role/role.component';
-=======
 import { TimerComponent } from './timer/timer.component';
 import { TchatComponent } from './tchat/tchat.component';
 import { AuthComponent } from './auth/auth.component';
@@ -14,20 +12,18 @@ import { AuthComponent } from './auth/auth.component';
 const appRoutes: Routes = [
   { path: 'login', component: AuthComponent },
   { path: 'test', component: TchatComponent },
+  { path: 'roletest', component: RoleComponent },
   { path: '',   redirectTo: '/login', pathMatch: 'full' }
 ];
->>>>>>> 64ab2b9bf8b82a3ccf01870ecfb0f277b64b9aef
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    RoleComponent
-=======
+    RoleComponent,
     TimerComponent,
     TchatComponent,
     AuthComponent,
->>>>>>> 64ab2b9bf8b82a3ccf01870ecfb0f277b64b9aef
   ],
   imports: [
     BrowserModule,
