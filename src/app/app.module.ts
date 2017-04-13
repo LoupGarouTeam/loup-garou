2c7c819f80f5a8bc9fe6b5b91951125b41f11d63
phlesaux <ph.lesaux@outlook.com>
2017-04-13 09:51:19 +0200

    Ajout component Lobby

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
@@ -9,6 +9,7 @@ import { RoleComponent } from './role/role.component';
 import { TimerComponent } from './timer/timer.component';
 import { TchatComponent } from './tchat/tchat.component';
 import { AuthComponent } from './auth/auth.component';
+import {LobbyComponent} from './lobby/lobby.component';
 import { BoardComponent } from './board/board.component';
 
 const appRoutes: Routes = [
@@ -23,23 +24,24 @@ const appRoutes: Routes = [
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RoleComponent,
-    TimerComponent,
-    TchatComponent,
-    AuthComponent,
-    BoardComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(appRoutes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-
+    declarations: [
+        AppComponent,
+        RoleComponent,
+        TimerComponent,
+        TchatComponent,
+        AuthComponent,
+        LobbyComponent,
+        BoardComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
